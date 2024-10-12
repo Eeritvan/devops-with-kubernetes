d597abc9a4bccb454e99b5a9e1476d55d7e02e72
Eeritvan <eeritvan@proton.me>
2024-10-12 20:37:18 +0300

    Added exercises 1.06 and 1.07

--- log output/build/index.js
+++ log output/build/index.js
@@ -1,8 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const uuid_1 = require("uuid");
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
 const time = new Date().toISOString();
 const string = (0, uuid_1.v4)();
-setInterval(() => {
-    console.log(`${time}: ${string}`);
-}, 5000);
+app.get('/', (_req, res) => {
+    res.send(`${time}: ${string}`);
+});
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
--- log output/index.ts
+++ log output/index.ts
@@ -1,8 +1,19 @@
 import { v4 as uuid } from 'uuid';
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
 
 const time: string = new Date().toISOString();
 const string: string = uuid();
 
-setInterval(() => {
-  console.log(`${time}: ${string}`);
-}, 5000);
+app.get('/', (_req: Request, res: Response) => {
+  res.send(`${time}: ${string}`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
