018c1df37d4f27b07a7f21467279c25177c7a2b5
Eeritvan <eeritvan@proton.me>
2024-10-13 21:49:10 +0300

    Added exercise 1.13

--- todo application/web app/build/index.js
+++ todo application/web app/build/index.js
@@ -10,7 +10,20 @@ const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.use('/image', express_1.default.static('image'));
 app.get('/', (_req, res) => {
-    res.send(`<img src="/image/image.jpg" style="width: 400px;">`);
+    res.send(`<div>
+              <div>
+                <img src="/image/image.jpg" style="width: 400px;">
+              </div>
+              <div>
+                <input> </input>
+                <Button> Create TODO </Button>
+              </div>
+              <ul>
+                <li> TODO 1 </li>
+                <li> TODO 2 </li>
+              </ul>
+            </div>
+  `);
 });
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
--- todo application/web app/index.ts
+++ todo application/web app/index.ts
@@ -9,7 +9,20 @@ const port = process.env.PORT;
 app.use('/image', express.static('image'));
 
 app.get('/', (_req: Request, res: Response) => {
-  res.send(`<img src="/image/image.jpg" style="width: 400px;">`);
+  res.send(`<div>
+              <div>
+                <img src="/image/image.jpg" style="width: 400px;">
+              </div>
+              <div>
+                <input> </input>
+                <Button> Create TODO </Button>
+              </div>
+              <ul>
+                <li> TODO 1 </li>
+                <li> TODO 2 </li>
+              </ul>
+            </div>
+  `);
 });
 
 app.listen(port, () => {
