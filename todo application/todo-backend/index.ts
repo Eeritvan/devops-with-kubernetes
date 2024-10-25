9ae12d6b3d5650ece025ed64004eaaca9700274c
Eeritvan <eeritvan@proton.me>
2024-10-25 21:46:40 +0300

    Added exercise 2.08

--- todo application/todo-backend/index.ts
+++ todo application/todo-backend/index.ts
@@ -1,30 +1,40 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import { Client } from 'pg';
 
 dotenv.config();
 
-const app: Express = express();
 const port = process.env.PORT;
+const app: Express = express();
+const client = new Client();
 
 app.use(express.urlencoded({ extended: true }));
 
-let todos = ['Finish homework', 'Study for exam', 'play games with friends'];
-
-app.get('/todos', (_req: Request, res: Response) => {
-  res.json(todos);
+app.get('/todos', async (_req: Request, res: Response) => {
+  try {
+    const result = await client.query(`SELECT task FROM todos;`);
+    const tasks = result.rows.map(row => row.task);
+    res.json(tasks);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
-app.post('/todos', (req: Request, res: Response) => {
-  todos.push(req.body.todo)
-
+app.post('/todos', async (req: Request, res: Response) => {
   const referer = req.get('Referer');
   if (referer) {
-    res.redirect(referer);
+    try {
+      await client.query(`INSERT INTO todos(task) VALUES ('${req.body.todo}');`);
+      res.redirect(referer);
+    } catch (e) {
+      console.error(e);
+    }
   } else {
     res.send("Error");
   }
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await client.connect();
   console.log(`App listening on port ${port}`);
 });
