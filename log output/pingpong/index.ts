5daae27eea9830c86e5216e91fccfac1f3075eef
Eeritvan <eeritvan@proton.me>
2024-10-25 20:27:08 +0300

    Added exercise 2.07

--- log output/pingpong/index.ts
+++ log output/pingpong/index.ts
@@ -1,32 +1,20 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
-import fs from 'node:fs/promises';
-import path from 'path';
+import { Client } from 'pg';
 
 dotenv.config();
 
-const app: Express = express();
 const port = process.env.PORT;
-
-const pongFilePath = path.join(__dirname, "/pongs.txt");
-
-const readPongs = async () => {
-  try {    
-    let count = Number(await fs.readFile(pongFilePath, 'utf8'));
-    count++;
-    await fs.writeFile(pongFilePath, count.toString(), 'utf8');
-    return Number(count);
-  } catch (err) {
-    await fs.writeFile(pongFilePath, (0).toString(), 'utf8');
-    return 0;
-  }
-};
+const app: Express = express();
+const client = new Client();
 
 app.get('/pingpong', async (_req: Request, res: Response) => {
-  const pongs = await readPongs();
+  const result = await client.query('UPDATE pings SET count = count + 1 WHERE id=1 RETURNING count;');
+  const pongs = result.rows[0].count;
   res.json({ pongs });
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await client.connect();
   console.log(`App listening on port ${port}`);
 });
